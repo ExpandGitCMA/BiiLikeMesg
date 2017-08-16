93ead3bf46d8fb0fa7fb0d608d0eefe072b6113c
voidAspire <1157213992@qq.com>
2017-08-16 16:54:34 +0800

    创建项目

---
+++ BiiLikeMesgEx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BiiLikeMesgEx
+//
+//  Created by 童公放 on 2017/8/16.
+//  Copyright © 2017年 ZeroSmile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
